116afe0fd97b8515654435d7138d72a00eed2663
Jason Travis <infomaniac7@gmail.com>
2013-01-21 16:05:58 -0700

    add: functions() prints the significand as a sum of decimal fractions.
    refactor: binary and its wrappers except for functions return a pointer
    to a global char array instead of printing directly.

--- main.c
+++ main.c
@@ -8,14 +8,20 @@
  */
 #include <stdio.h>
 
-void exponent(int);
-void mantissa(int);
-void binary(int, int, int);
 void printAnalysis(int);
+char* exponent(int);
+char* mantissa(int);
+void fractions(int);
+char* binary(char*, int, int);
 
-int main() {
+// string representations
+char expo[8]; // binary exponent
+char sig[23]; // binary significand
+
+int main()
+{
 	float f;
-	
+
 	printf("Enter real number: ");
 
 	scanf("%f", &f);
@@ -30,23 +36,21 @@ int main() {
 
 	return 0;
 }
-
 /*
- * printAnalysis prints a summary of the float bits components
+ * printAnalysis prints a summary of the float components
  */
-void printAnalysis(int bits) {
+void printAnalysis(int bits)
+{
 	int sign = (bits >> 31) & 1;
 	int exp = (bits >> 23) & 255;
 
 	printf("Float Analysis\n");
-	
-	printf("  Bit Pattern:\t%d ", sign);
-	exponent(bits); printf(" ");
-	mantissa(bits); printf("\n");
+
+	printf("  Bit Pattern:\t%d %s %s\n", sign, exponent(bits), mantissa(bits));
 	printf("\t\tS Exponent Significand/Mantissa\n\n");
 
 	// interpret sign as either 'positive' or 'negative'
-	char  *s;
+	char *s;
 	if(sign == 0) {
 		s = "(positive)";
 	} else {
@@ -54,36 +58,52 @@ void printAnalysis(int bits) {
 	}
 	printf("  Sign:\t\t%d %s\n", sign, s);
 
-	printf("  Exponent:\t");
-	exponent(bits);
-	printf(" = %d; w/bias 127 -> (%d-127) = %d\n", exp, exp, exp - 127);
-	
-	printf("  Significand:\t");
-	mantissa(bits); printf("\n");
-	printf("   w/implied 1: 1.");
-	mantissa(bits); printf("\n");
-	
-	printf("  Combined:\t+ [1.");
-	mantissa(bits);
+	printf("  Exponent:\t%s = %d; w/bias 127 -> (%d-127) = %d\n", exponent(bits), exp, exp, exp - 127);
+
+	printf("  Significand:\t%s\n", mantissa(bits));
+	printf("   w/implied 1: 1.%s\n", mantissa(bits));
+
+	printf("  Combined:\t+ [1.%s] * 2^%d\n", mantissa(bits) , exp - 127);
+	printf("   or:\t\t+ [");
+	fractions(bits);
 	printf("] * 2^%d\n", exp - 127);
-	printf("   or:\n");
-	// TODO: + [1 + (1/2) + (1/64)] * 2^3
 }
 
 /*
- * exponent is a wrapper around binary that prints the exponent
- * piece of the float bits
+ * exponent is a wrapper around binary that stores a binary
+ * representation of the exponent in the expo global variable
+ * and returns the pointer.
  */
-void exponent(int bits) {
-	binary(bits, 30, 8);
+char* exponent(int bits)
+{
+	return binary(expo, bits >> 23, 8);
 }
 
 /*
- * mantissa is a wrapper around binary that prints the significand
- * piece of the float bits
+ * mantissa is a wrapper around binary that stores a binary
+ * representation of the significand in the sig global variable
+ * and returns the pointer.
  */
-void mantissa(int bits) {
-	binary(bits, 22, 22);
+char* mantissa(int bits)
+{
+	return binary(sig, bits, 23);
+}
+
+/*
+ * fractions is a wrapper around binary that prints the significand
+ * as a sum of decimal fractions.
+ */
+void fractions(int bits)
+{
+	//binary(sig, bits, 23); sig should already be populated by mantissa
+	// 1 + 1/2^(i+1) + ...
+	printf("1");
+	int i;
+	for(i = 0; i < 23; i++) {
+		if(sig[i] - '0') {
+			printf(" + (1/%d)", (sig[i] - '0') << i + 1);
+		}
+	}
 }
 
 /*
@@ -92,9 +112,13 @@ void mantissa(int bits) {
  * high is the index of the highest order bit in the range.
  * len is the length of the range.
  */
-void binary(int bits, int high, int len) {
+char* binary(char *dst, int bits, int len)
+{
 	int i;
-	for(i = high; i > (high - len); i--) {
-		printf("%d", (bits >> i) & 1);
+	for(i = 0; i < len; i++) {
+		// shift index 'i' bit all the way to the right,
+		// clear all other bits, and shift to its ascii value.
+		dst[i] = ((bits >> (len - 1 - i) & 1) + '0');
 	}
+	return dst;
 }
